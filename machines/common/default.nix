375a18bc9a3017a0f017523ca771ffd38bb56ae4
Jacob <admin@kath.lol>
2026-01-03 21:30:11 +0800

    better nuke ipv6
    holy fucking shit i hate you nixos PLEASE JUST LET ME TURN OFF IPV6

--- machines/common/default.nix
+++ machines/common/default.nix
@@ -13,6 +13,18 @@
     enableIPv6 = false;
   };
 
+  boot = {
+    kernel = {
+      sysctl = {
+        "net.ipv6.conf.all.disable_ipv6" = "1";
+        "net.ipv6.conf.default.disable_ipv6" = "1";
+        "net.ipv6.conf.lo.disable_ipv6" = "1";
+      };
+    };
+    kernelParams = [ "ipv6.disable=1"];
+  };
+
+
   nixpkgs = {
     config = {
       allowUnfree = true;
