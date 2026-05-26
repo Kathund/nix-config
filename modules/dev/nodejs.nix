5b93db543f9412df16345627ef6dd7642acb269d
Jacob <kathund@proton.me>
2026-05-26 10:05:11 +0800

    update: 7tv

--- modules/dev/nodejs.nix
+++ modules/dev/nodejs.nix
@@ -11,12 +11,19 @@ in
 {
   options.modules.dev.${program} = {
     enable = lib.mkEnableOption { description = "Enable ${program}"; };
+    zsh = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
   };
 
   config = lib.mkIf cfg.enable {
     home-manager.users.${username} =
       { pkgs, ... }:
       {
+        programs.zsh.shellAliases = lib.mkIf cfg.zsh {
+          pnpmCheck = "pnpm prettier && pnpm eslint && pnpm build";
+        };
         home.packages = with pkgs; [
           nodejs_24
           pnpm
